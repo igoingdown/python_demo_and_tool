b0bc55284c19054dfcd58d9ee6afe0e0e9cd00e9
igoingdown <fycjmingxing@126.com>
2017-01-15 15:45:27 +0800

    commit time: 2017-01-15 15:45:27

--- CommitModificationOnGIthubRepositoriesCasually.sh
+++ CommitModificationOnGIthubRepositoriesCasually.sh
@@ -1,51 +1,24 @@
 #!/usr/bin/env bash
 
-# every time after i modified some files in the following repositories,
-# execute this bash script will be OK!
-# In this way, the auto_commit.sh file doesn't need duplicates any more actually!
+# Every time after i modified some files in the following repositories,
+# Execute this bash script will be OK!
+# In this way, the auto_commit.sh file doesn't need duplicates any more.
 
-tensorflow_project_path="/Users/zhaomingxing/PycharmProjects/test_tensor"
-java_demo_path="/Users/zhaomingxing/IdeaProjects/hello_world/src/package_1"
 python_demo_and_tool_path="/Users/zhaomingxing/PycharmProjects/python_demo_and_tool"
-leetcode_path="/Users/zhaomingxing/Desktop/zmx/leetcode/leetcode_duplicate"
-handbook_path="/Users/zhaomingxing/Desktop/zmx/Interesting-Github-Repositories/PythonDataScienceHandbook"
-vzch_toy_path="/Users/zhaomingxing/Desktop/zmx/Interesting-Github-Repositories/vczh_toys"
-google_java_path="/Users/zhaomingxing/Desktop/zmx/Interesting-Github-Repositories/google-maps-services-java"
-resume_path="/Users/zhaomingxing/Desktop/zmx/MyResume"
-nn_models_path="/Users/zhaomingxing/PycharmProjects/models"
-copy_net_seq2seq_model_path="/Users/zhaomingxing/PycharmProjects/copyNet"
 
-cd $tensorflow_project_path
-bash "$python_demo_and_tool_path/auto_commit.sh"
+path_arr=(
+	"/Users/zhaomingxing/IdeaProjects/hello_world/src/package_1" 
+	"/Users/zhaomingxing/Desktop/zmx/leetcode/leetcode_duplicate" 
+	"/Users/zhaomingxing/Desktop/zmx/Interesting-Github-Repositories/google-maps-services-java" 
+	"/Users/zhaomingxing/Desktop/zmx/MyResume" 
+	"/Users/zhaomingxing/PycharmProjects/models" 
+	"/Users/zhaomingxing/PycharmProjects/copyNet" 
+	"/Users/zhaomingxing/PycharmProjects/python_demo_and_tool" 
+	"/Users/zhaomingxing/PycharmProjects/test_tensor")
 
-cd $java_demo_path
-bash "$python_demo_and_tool_path/auto_commit.sh"
-
-cd $python_demo_and_tool_path
-bash "$python_demo_and_tool_path/auto_commit.sh"
-
-
-cd $leetcode_path
-bash "$python_demo_and_tool_path/auto_commit.sh"
-
-
-cd $handbook_path
-bash "$python_demo_and_tool_path/auto_commit.sh"
-
-
-cd $vzch_toy_path
-bash "$python_demo_and_tool_path/auto_commit.sh"
-
-
-cd $google_java_path
-bash "$python_demo_and_tool_path/auto_commit.sh"
-
-cd $resume_path
-bash "$python_demo_and_tool_path/auto_commit.sh"
-
-cd $copy_net_seq2seq_model_path
-bash "$python_demo_and_tool_path/auto_commit.sh"
-
-cd $nn_models_path
-bash "$python_demo_and_tool_path/auto_commit.sh"
+for path in ${path_arr[@]}
+do
+	cd $path
+	bash "$python_demo_and_tool_path/auto_commit.sh"
+done
 
--- README.md
+++ README.md
@@ -11,9 +11,13 @@ My gpa table.
 # lda-test.py
 Just test LDA model using demo dataset.
 
-#auto-commit.sh
+# auto-commit.sh
 Use shell scripts to commit modifications automatically. It's easy and convenient.
 
-#shell-demo-change-script-files-into-executable.sh
+# shell-demo-change-script-files-into-executable.sh
 Modify permission of normal users. Give them execution permission.
 
+# CommitModificationOnGIthubRepositoriesCasually.sh
+I have several github repositories.
+I have to do the same operations on the repositories because I usually don't know what i modified clear.
+An idea occurred to me to use shell script to do these duplicate jobs automatically.
