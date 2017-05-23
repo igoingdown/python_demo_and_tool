d0d7a178dd472245c6628391bb1896885b6166f3
igoingdown <fycjmingxing@126.com>
2017-05-23 23:07:12 +0800

    commit time: 2017-05-23 23:07:12

---
+++ test_gensim.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+===============================================================================
+author: 赵明星
+desc:   测试gensim包的简单功能。
+===============================================================================
+"""
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import gensim
+import jieba
+
+def foo():
+    lines = []
+    with open("weibo_texts/test.txt") as f:
+        for line in f:
+            lines.append(jieba.cut(line))
+    d = gensim.corpora.Dictionary(lines)
+    for word, index in d.token2id.iteritems():
+        print word, index
+
+
+if __name__ == '__main__':
+    foo()
