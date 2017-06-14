1edc237d06b3e9905b57ecb77443a6b952abe94d
igoingdown <fycjmingxing@126.com>
2017-06-14 11:56:56 +0800

    a new version of  auto commit add. the new version need an commit arg and commit time: 2017-06-14 11:56:56.

---
+++ auto_commit_need_commit_args.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+working_dir=`pwd`
+echo -e "\n\n\n\ntrying to commit modification in $working_dir\n"
+
+git status
+git add .
+s_1=`date +%F`
+s_2=`date +%T`
+remoteOrigin=`git remote`
+git commit -m "$1 and commit time: $s_1 $s_2."
+git push -u "$remoteOrigin" master
+
+echo -e "\nmodification in $working_dir commit over!\n\n\n\n"
--- shell_test.sh
+++ shell_test.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
 echo "aa"
+echo $1
+echo "$1"
