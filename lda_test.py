ccea82d8471119fd0789489e5e10f39dadeb1cbe
igoingdown <fycjmingxing@126.com>
2017-10-11 14:06:11 +0800

    and commit time: 2017-10-11 14:06:11.

--- lda_test.py
+++ lda_test.py
@@ -23,11 +23,12 @@ titles = lda.datasets.load_reuters_titles()
 print "X.shape = {}".format(X.shape)
 print "X.sum = {}".format(X.sum())
 
-model = lda.LDA(n_topics=20, n_iters=1000, random_state=1)
+m = lda.LDA()
+model = lda.LDA(n_topics=20, n_iter=1000, random_state=1)
 model.fit(X)
 
 topic_word = model.topic_word_
 n_top_word = 8
 for i, topic_dist in enumerate(topic_word):
 	topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n_top_word+1):-1]
-	print "topic {0}:{1}".format(i, ''.join(topic_words)
+	print "topic {0}:{1}".format(i, ''.join(topic_words))
