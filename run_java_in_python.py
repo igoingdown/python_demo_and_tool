d11a679b33427cee98582aa17691521510f56a4b
igoingdown <fycjmingxing@126.com>
2017-10-11 19:57:25 +0800

    and commit time: 2017-10-11 19:57:25.

--- run_java_in_python.py
+++ run_java_in_python.py
@@ -4,6 +4,20 @@
 """
 ===============================================================================
 author: 赵明星
-desc:   抓取有意思的图片。
+desc:   在python中尝试调用java类，很遗憾Mac不支持jpype。
+        直接用spring boot把java类做成web服务也行。
 ===============================================================================
 """
+
+# from jpype import *
+import os.path
+
+def foo():
+
+    # startJVM("C:/Java/jdk1.6.0_10/jre/bin/client/jvm.dll", "-ea")
+    # java.lang.System.out.println("hello World")
+    # shutdownJVM()
+    pass
+
+if __name__ == '__main__':
+    foo()
