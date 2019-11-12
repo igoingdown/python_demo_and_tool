0d191f15e80141ffb0c00416f5fe4dc5acab25a4
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2019-11-13 00:22:57 +0800

    update the income calculator

--- tools/income_calculator/calculate.py
+++ tools/income_calculator/calculate.py
@@ -28,37 +28,40 @@ def find_tax_range(before_tax_income_sum):
         return v
 
 
+# raw_salary = society_insurance + base_allowance + tax + taxed_salary
+# base_raw_salary 表示较为固定的税前月薪
+# actual_raw_salary 表示实际的一个月的税前月薪，适用于每月税前薪资波动的场景
+
 class Calculator(object):
-    def __init__(self, base_salary, society_insurance,
-                 no_tax_base_income, registered_house_rent):
-        self.base_salary = base_salary
+    def __init__(self, base_raw_salary, society_insurance, registered_house_rent):
+        self.base_raw_salary = base_raw_salary
         self.society_insurance = society_insurance
-        self.no_tax_base_income = no_tax_base_income
         self.registered_house_rent = registered_house_rent
+        self.house_rent_allowance = 1000.0
+        self.base_allowance = 5000.0
 
     def calculate_Nth_month_salary(self, n):
-        before_tax_income_sum = self.base_salary * n - self.society_insurance * n - 5000.0 * n
+        before_tax_income_sum = self.base_raw_salary * n - self.society_insurance * n - self.base_allowance * n
         if self.registered_house_rent:
-            before_tax_income_sum -= 1000.0 * n
+            before_tax_income_sum -= self.house_rent_allowance * n
         # 查dict，找出税率和速算扣除数
         rate, reduce_num = find_tax_range(before_tax_income_sum)
         tax_sum = before_tax_income_sum * rate - reduce_num
         return tax_sum
 
-    def calculate_next_month_salary(self, salary_totals, tax_records):
-        before_tax_income_sum = sum(salary_totals, self.base_salary) - (self.society_insurance + 5000.0) * (len(salary_totals) + 1)
+    def calculate_next_month_salary(self, previous_actual_raw_salary_list, tax_record_list):
+
+        before_tax_income_sum = sum(previous_actual_raw_salary_list, self.base_raw_salary) - (self.society_insurance + self.base_allowance) * (len(previous_actual_raw_salary_list) + 1)
         if self.registered_house_rent:
-            before_tax_income_sum -= 1000.0 * (1 + len(salary_totals))
+            before_tax_income_sum -= self.house_rent_allowance * (1 + len(previous_actual_raw_salary_list))
         # 查dict，找出税率和速算扣除数
         rate, reduce_num = find_tax_range(before_tax_income_sum)
         tax_sum = before_tax_income_sum * rate - reduce_num
-        return -sum(tax_records, -tax_sum)
+        return -sum(tax_record_list, -tax_sum)
 
     def print_annual_income_curve(self):
         last_tax_sum = 0.0
-        for i in range(13):
-            if i == 0:
-                continue
+        for i in range(1, 13):
             # 计算每月的收入
             cur_tax_sum = self.calculate_Nth_month_salary(i)
             print(cur_tax_sum - last_tax_sum)
