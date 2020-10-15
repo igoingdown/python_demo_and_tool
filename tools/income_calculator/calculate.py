36f9de401231fdba4e53d5edc33bce95a026e57c
zhaomingxing.93 <zhaomingxing.93@bytedance.com>
2020-10-15 20:22:48 +0800

    [fix]更新计算逻辑

--- tools/income_calculator/calculate.py
+++ tools/income_calculator/calculate.py
@@ -7,67 +7,112 @@ desc:   计算新个税下的个人收入。
 ===============================================================================
 """
 
-
-# a dict for limit to tax rage and reduce num
-d = {
-    36000.0: (0.03, 0),
-    144000.0: (0.1, 2520),
-    300000.0: (0.2, 16920),
-    420000.0: (0.25, 31920),
-    660000.0: (0.3, 52920),
-    960000.0: (0.35, 85920),
-    100000000000000000: (0.45, 181920)
-}
+from tools.income_calculator.const import *
 
 
 def find_tax_range(before_tax_income_sum):
-    for k, v in d.items():
+    for k, v in salary_tax_rate_dict.items():
         pass
         if before_tax_income_sum > k:
             continue
         return v
 
 
-# raw_salary = society_insurance + base_allowance + tax + taxed_salary
-# base_raw_salary 表示较为固定的税前月薪
-# actual_raw_salary 表示实际的一个月的税前月薪，适用于每月税前薪资波动的场景
+class Insurance(object):
+    def __init__(self, rate, min_base, max_base, fee_base):
+        self.rate = rate
+        self.max_base = max_base
+        self.min_base = min_base
+        self.fee_base = fee_base
+
+    def get_fee(self, base):
+        fee = base * self.rate
+        if base < self.min_base:
+            fee = self.min_base * self.rate
+        if base > self.max_base:
+            fee = self.max_base * self.rate
+        fee += self.fee_base
+        return fee
+
+
+class Allowance(object):
+    def __init__(self, base_fee, total_fee):
+        self.base_fee = base_fee
+        self.total_fee = total_fee
+
+    def get_fee(self, i):
+        if i * self.base_fee <= self.total_fee:
+            return self.base_fee
+        else:
+            return 0
+
+# raw_salary: 当月税前月薪
+# last_year_average_raw_salary: 去年平均月薪，每年7月更新
+# housing_fund: 公积金
+# pension: 养老保险
+# unemployment_insurance: 失业保险
+# housing_fund_fee(公积金总数) = last_year_average_raw_salary * housing_fund_rage
+# taxed_salary = raw_salary - insurance_fee - housing_fund_fee - tax
+# base_tax_free_salary: 每月免税金额,每月￥5000
+# tax_free_salary(每月实际免税收入) = base_tax_free_salary + tax_free_allowance
+# tax_free_allowance: 免税津贴,如租房个税减免 
+# tax_free_house_rent_allowance: 租房个税减免金额，￥1000
+# donation_tax_free_salary: 捐赠个税减免
+# tax: 个税
+# taxed_salary: 每月税后收入
+# 公积金基数上线:
+
 
 class Calculator(object):
-    def __init__(self, base_raw_salary, society_insurance, registered_house_rent):
-        self.base_raw_salary = base_raw_salary
-        self.society_insurance = society_insurance
-        self.registered_house_rent = registered_house_rent
-        self.house_rent_allowance = 1000.0
-        self.base_allowance = 5000.0
+    def __init__(self, registered_house_rent):
+        self.housing_fund = Insurance(HOUSING_FUND_RATE, MIN_BASE_HOUSING_FUND, MAX_BASE_HOUSING_FUND, BASE_FEE_DEFAULT)
+        self.unemployment_insurance = Insurance(UNEMPLOYMENT_INSURANCE_RATE, MIN_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION, MAX_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION, BASE_FEE_DEFAULT)
+        self.pension = Insurance(PENSION_RATE, MIN_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION, MAX_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION, BASE_FEE_DEFAULT)
+        self.medical_insurance = Insurance(MEDICAL_INSURANCE_RATE, MIN_BASE_MEDICAL_INSURANCE, MAX_BASE_MEDICAL_INSURANCE, BASE_FEE_MEDICAL)
+        self.house_rent_allowance = Allowance(tax_free_house_rent_allowance, annual_tax_free_house_rent_allowance)
 
-    def calculate_Nth_month_salary(self, n):
-        before_tax_income_sum = self.base_raw_salary * n - self.society_insurance * n - self.base_allowance * n
-        if self.registered_house_rent:
-            before_tax_income_sum -= self.house_rent_allowance * n
+    '''
+    calculate_tax_sum 计算税费总数
+    '''
+    def calculate_tax_sum(self, registered_insurance_base_list, raw_salary_list, extra_tax_free_salary):
+        if len(registered_insurance_base_list) != len(raw_salary_list):
+            return 0
+        before_tax_income_sum = sum(raw_salary_list)
+        before_tax_income_sum -= sum(extra_tax_free_salary)
+        before_tax_income_sum -= sum([base_tax_free_salary] * len(raw_salary_list))
+        before_tax_income_sum -= sum([self.pension.get_fee(x) for x in registered_insurance_base_list])
+        before_tax_income_sum -= sum([self.unemployment_insurance.get_fee(x) for x in registered_insurance_base_list])
+        before_tax_income_sum -= sum([self.medical_insurance.get_fee(x) for x in registered_insurance_base_list])
+        before_tax_income_sum -= sum([self.housing_fund.get_fee(x) for x in registered_insurance_base_list])
+        before_tax_income_sum -= sum([self.house_rent_allowance.get_fee(x+1) for x in range(len(raw_salary_list))])
+
         # 查dict，找出税率和速算扣除数
         rate, reduce_num = find_tax_range(before_tax_income_sum)
         tax_sum = before_tax_income_sum * rate - reduce_num
         return tax_sum
 
-    def calculate_next_month_salary(self, previous_actual_raw_salary_list, tax_record_list):
+    '''
+    calculate_every_month_salary: 给定税前收入列表，输出每月税后收入和个税
+    '''
+    def calculate_every_month_salary(self, registered_insurance_base_list, raw_salary_list, extra_tax_free_salary):
+        if len(registered_insurance_base_list) != len(raw_salary_list) or len(raw_salary_list) != len(extra_tax_free_salary):
+            return
+        cur_tax_sum = 0
+        for i in range(len(raw_salary_list)):
+            tax_sum = self.calculate_tax_sum(registered_insurance_base_list[:i+1], raw_salary_list[:i+1], extra_tax_free_salary[:i+1])
+            yield tax_sum - cur_tax_sum
+            cur_tax_sum = tax_sum
 
-        before_tax_income_sum = sum(previous_actual_raw_salary_list, self.base_raw_salary) - (self.society_insurance + self.base_allowance) * (len(previous_actual_raw_salary_list) + 1)
-        if self.registered_house_rent:
-            before_tax_income_sum -= self.house_rent_allowance * (1 + len(previous_actual_raw_salary_list))
-        # 查dict，找出税率和速算扣除数
-        rate, reduce_num = find_tax_range(before_tax_income_sum)
-        tax_sum = before_tax_income_sum * rate - reduce_num
-        return -sum(tax_record_list, -tax_sum)
-
-    def print_annual_income_curve(self):
-        last_tax_sum = 0.0
-        for i in range(1, 13):
-            # 计算每月的收入
-            cur_tax_sum = self.calculate_Nth_month_salary(i)
-            print(cur_tax_sum - last_tax_sum)
-            last_tax_sum = cur_tax_sum
+    def print_tax_curve(self, registered_insurance_base_list, raw_salary_list, extra_tax_free_salary):
+        for tax in self.calculate_every_month_salary(registered_insurance_base_list, raw_salary_list, extra_tax_free_salary):
+            print(tax)
 
 
 if __name__ == "__main__":
-    c = Calculator(36000, 6000, False)
-    c.print_annual_income_curve()
+    c = Calculator(True)
+    salary_list = [26000] * 12
+    insurance_base_list = [20000] * 6 + [23967] * (len(salary_list) - 6)
+    donation_tax_free_salary = [0] * 3
+    donation_tax_free_salary.append(500)
+    donation_tax_free_salary += [0] * (len(salary_list) - 4)
+    c.print_tax_curve(insurance_base_list, salary_list, donation_tax_free_salary )
---
+++ tools/income_calculator/const.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+"""
+===============================================================================
+author: 赵明星
+desc:   计算新个税下的个人收入所需的常量列表。
+===============================================================================
+"""
+
+# MIN_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION 养老金和失业保险的缴纳基数下限
+MIN_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION = 3613
+# MAX_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION 养老金和失业保险的缴纳基数上限
+MAX_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION = 26541
+# UNEMPLOYMENT_INSURANCE_RATE 失业保险的个人缴纳比例
+UNEMPLOYMENT_INSURANCE_RATE = 0.002
+# PENSION_RATE 养老金的个人缴纳比例
+PENSION_RATE = 0.08
+
+
+# MIN_BASE_MEDICAL_INSURANCE 医疗保险的缴纳基数下限
+MIN_BASE_MEDICAL_INSURANCE = 5360
+# MAX_BASE_MEDICAL_INSURANCE 医疗保险的缴纳基数上限
+MAX_BASE_MEDICAL_INSURANCE = 29732
+# MEDICAL_INSURANCE_RATE 医疗保险的个人缴纳比例
+MEDICAL_INSURANCE_RATE = 0.02
+
+
+# MIN_BASE_HOUSING_FUND 住房公积金的缴纳基数下限
+MIN_BASE_HOUSING_FUND = 2200
+# MAX_BASE_HOUSING_FUND 住房公积金的缴纳基数上限
+MAX_BASE_HOUSING_FUND = 27786
+# HOUSING_FUND_RATE 住房公积金个人缴纳比例
+HOUSING_FUND_RATE = 0.12
+
+
+# BASE_FEE_DEFAULT 除医疗保险外四险一金最低缴纳金额
+BASE_FEE_DEFAULT = 0
+# BASE_FEE_MEDICAL 医疗保险最低缴纳金额
+BASE_FEE_MEDICAL = 3
+
+
+# tax_free_house_rent_allowance 个税租房专项减免单月金额
+tax_free_house_rent_allowance = 1500
+# annual_tax_free_house_rent_allowance 个税租房专项减免全年总金额上限
+annual_tax_free_house_rent_allowance = 12000
+# base_tax_free_salary 个税起征点月薪
+base_tax_free_salary = 5000.0
+
+
+# salary_tax_rate_dict a dict for limit to tax rage and reduce num
+salary_tax_rate_dict = {
+    36000.0: (0.03, 0),
+    144000.0: (0.1, 2520),
+    300000.0: (0.2, 16920),
+    420000.0: (0.25, 31920),
+    660000.0: (0.3, 52920),
+    960000.0: (0.35, 85920),
+    100000000000000000: (0.45, 181920)
+}
