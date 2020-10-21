f974e7142cefaa55ac42733eb01e8206789ff1fe
shlang <shlang@microsoft.com>
2020-10-21 21:58:16 +0800

    add choice

--- tools/income_calculator/calculate.py
+++ tools/income_calculator/calculate.py
@@ -8,7 +8,7 @@ desc:   计算新个税下的个人收入。
 """
 
 from tools.income_calculator.const import *
-
+import math
 
 def find_tax_range(before_tax_income_sum):
     for k, v in salary_tax_rate_dict.items():
@@ -26,11 +26,8 @@ class Insurance(object):
         self.fee_base = fee_base
 
     def get_fee(self, base):
+        base = max(min(base, self.max_base), self.min_base)
         fee = base * self.rate
-        if base < self.min_base:
-            fee = self.min_base * self.rate
-        if base > self.max_base:
-            fee = self.max_base * self.rate
         fee += self.fee_base
         return fee
 
@@ -55,7 +52,7 @@ class Allowance(object):
 # taxed_salary = raw_salary - insurance_fee - housing_fund_fee - tax
 # base_tax_free_salary: 每月免税金额,每月￥5000
 # tax_free_salary(每月实际免税收入) = base_tax_free_salary + tax_free_allowance
-# tax_free_allowance: 免税津贴,如租房个税减免 
+# tax_free_allowance: 免税津贴,如租房个税减免
 # tax_free_house_rent_allowance: 租房个税减免金额，￥1000
 # donation_tax_free_salary: 捐赠个税减免
 # tax: 个税
@@ -64,12 +61,18 @@ class Allowance(object):
 
 
 class Calculator(object):
-    def __init__(self, registered_house_rent):
+    def __init__(self, registered_house_rent, housing_fund_needed=True, unemployment_insurance_needed=True,
+                 pension_needed=True, medical_insurance_needed=True):
         self.housing_fund = Insurance(HOUSING_FUND_RATE, MIN_BASE_HOUSING_FUND, MAX_BASE_HOUSING_FUND, BASE_FEE_DEFAULT)
         self.unemployment_insurance = Insurance(UNEMPLOYMENT_INSURANCE_RATE, MIN_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION, MAX_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION, BASE_FEE_DEFAULT)
         self.pension = Insurance(PENSION_RATE, MIN_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION, MAX_BASE_UNEMPLOYMENT_INSURANCE_AND_PENSION, BASE_FEE_DEFAULT)
         self.medical_insurance = Insurance(MEDICAL_INSURANCE_RATE, MIN_BASE_MEDICAL_INSURANCE, MAX_BASE_MEDICAL_INSURANCE, BASE_FEE_MEDICAL)
         self.house_rent_allowance = Allowance(tax_free_house_rent_allowance, annual_tax_free_house_rent_allowance)
+
+        self.housing_fund_needed = housing_fund_needed
+        self.unemployment_insurance_needed = unemployment_insurance_needed
+        self.pension_needed = pension_needed
+        self.medical_insurance_needed = medical_insurance_needed
 
     '''
     calculate_tax_sum 计算税费总数
@@ -80,10 +83,14 @@ class Calculator(object):
         before_tax_income_sum = sum(raw_salary_list)
         before_tax_income_sum -= sum(extra_tax_free_salary)
         before_tax_income_sum -= sum([base_tax_free_salary] * len(raw_salary_list))
-        before_tax_income_sum -= sum([self.pension.get_fee(x) for x in registered_insurance_base_list])
-        before_tax_income_sum -= sum([self.unemployment_insurance.get_fee(x) for x in registered_insurance_base_list])
-        before_tax_income_sum -= sum([self.medical_insurance.get_fee(x) for x in registered_insurance_base_list])
-        before_tax_income_sum -= sum([self.housing_fund.get_fee(x) for x in registered_insurance_base_list])
+        if self.pension_needed:
+            before_tax_income_sum -= sum([self.pension.get_fee(x) for x in registered_insurance_base_list])
+        if self.unemployment_insurance_needed:
+            before_tax_income_sum -= sum([self.unemployment_insurance.get_fee(x) for x in registered_insurance_base_list])
+        if self.medical_insurance_needed:
+            before_tax_income_sum -= sum([self.medical_insurance.get_fee(x) for x in registered_insurance_base_list])
+        if self.housing_fund_needed:
+            before_tax_income_sum -= sum([self.housing_fund.get_fee(x) for x in registered_insurance_base_list])
         before_tax_income_sum -= sum([self.house_rent_allowance.get_fee(x+1) for x in range(len(raw_salary_list))])
 
         # 查dict，找出税率和速算扣除数
